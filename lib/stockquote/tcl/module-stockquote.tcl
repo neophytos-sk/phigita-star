c131090dc2f729f196776469d4515330030eeb22
Neophytos Demetriou <neophytos@gmail.com>
2013-08-11 14:24:32 +0300

    (a) created new lib module tdom_procs having xpathfunc and html extractor procs, (b) cleanup of curl module, (c) a test file that fetches a news page and extracts links

--- lib/stockquote/tcl/module-stockquote.tcl
+++ lib/stockquote/tcl/module-stockquote.tcl
@@ -724,67 +724,6 @@ proc ::ext::StockQuote::CSE.get_daily_prices {metadataVar dataVar exchange {date
     return $errorcode
 }
 
-namespace eval ::xo::http {;}
-
-proc ::xo::http::fetch {contentVar url} {
-    upvar ${contentVar} content
-
-    set errorcode [curl::transfer -url $url \
-		       -bodyvar content \
-		       -infohttpcode httpcode \
-		       -infocontenttype contenttype]
-
-
-    if { $errorcode == 0 && $httpcode == 200} {;}
-
-    return $errorcode
-}
-
-namespace eval ::xo::html {;}
-proc ::xo::html::extract {outResultVar inHtmlVar xpath {domNodeFunc "asHTML"} {xpathFunc ""}} {
-    upvar $outResultVar result
-    upvar $inHtmlVar html
-
-    set doc [dom parse -html $html]
-    if { $xpathFunc ne {} } {
-	set xpath "${xpathFunc}($xpath)"
-    }
-
-    set nodes [$doc selectNodes $xpath]
-
-    if { $domNodeFunc ne {} } {
-	set result [list]
-	foreach node $nodes {
-	    lappend result [$node {*}${domNodeFunc}]
-	}
-    } else {
-	set result $nodes
-    }
-
-    $doc delete
-}
-
-# tDOM: asText converts {&nbsp;} to \xa0
-proc ::xo::html::table_to_multilist {dataVar htmltableVar} {
-    upvar $dataVar data
-    upvar $htmltableVar htmltable
-
-    set doc [dom parse -html $htmltable]
-    set xpath "//tr"
-    set nodes [$doc selectNodes $xpath]
-
-    set data [list]
-    foreach node $nodes {
-	set row [list]
-	foreach childNode [$node childNodes] {
-	    lappend row [string trim [$childNode asText] " \xa0"]
-	}
-	lappend data $row
-    }
-    $doc delete
-
-}
-
 proc ::ext::StockQuote::Xak.get_intraday_prices {metadataVar dataVar exchange symbol date} {
     upvar $metadataVar metadata
     upvar $dataVar data
