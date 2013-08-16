8c1f272f34e9de03e27ca7dd67c8d11da3d544e3
Neophytos Demetriou <neophytos@gmail.com>
2013-08-17 02:22:08 +0300

    feed reader: added uses-content usage info

--- lib/curl/test/feed-reader.tcl
+++ lib/curl/test/feed-reader.tcl
@@ -216,6 +216,7 @@ proc print_usage_info {} {
 			   "show" "urlsha1" \
 			   "show-url" "article_url" \
 			   "show-content" "contentsha1" \
+			   "uses-content" "contentsha1" \
 			   "list" "feed_name" \
 			   "TODO:test-article" "article_url" \
 			   "TODO:add" "feed_url"]
