789d8ef4bf6f81745312e6447ac9e7ffe41eac88
Neophytos Demetriou <neophytos@gmail.com>
2013-08-18 15:40:56 +0300

    redirect for registration saga (continued)

--- packages/core-platform/tcl/request-processor/rp-filter-procs.tcl
+++ packages/core-platform/tcl/request-processor/rp-filter-procs.tcl
@@ -204,7 +204,7 @@ Object create ::xo::defaultRequestFilter
     set user_id [ad_conn user_id]
     ad_conn_set screen_name [::xo::kit::get_screen_name $user_id]
 
-    ns_log notice "peeraddr=[ad_conn peeraddr] user_id=[ad_conn user_id]"
+    ns_log notice "peeraddr=[ad_conn peeraddr] user_id=[ad_conn user_id] url=[ns_conn url]"
 
 
     #####
