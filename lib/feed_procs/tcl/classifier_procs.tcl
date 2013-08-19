79b3f2333e4a51f471a681741c736523757e6554
Neophytos Demetriou <neophytos@gmail.com>
2013-08-20 01:41:40 +0300

    feed reader: commit new files (related to earlier comments)

---
+++ lib/feed_procs/tcl/classifier_procs.tcl
@@ -0,0 +1,171 @@
+namespace eval ::feed_reader::classifier {;}
+
+proc ::feed_reader::classifier::get_classifier_dir {} {
+    return [::feed_reader::get_base_dir]/classifier
+}
+
+proc ::feed_reader::classifier::register_axis {axis} {
+
+    set re {^[[:alpha:]]{2}.utf8_[[:alnum:]]+$}
+    if { ![regexp -- ${re} ${axis}] } {
+	error "axis name must be of the form lang.encoding.alnum, for example, el.utf8.topic"
+    }
+
+    set classifier_dir [get_classifier_dir]
+    set axis_dir ${classifier_dir}/${axis}
+
+    file mkdir ${axis_dir}
+
+}
+
+proc ::feed_reader::classifier::get_axis_names {} {
+   set classifier_dir [get_classifier_dir]
+    return [lsort [glob -tails -directory ${classifier_dir} *]]
+}
+
+proc ::feed_reader::classifier::get_label_names {axis} {
+    set axis_dir [get_classifier_dir]/${axis}
+    return [lsort [glob -tails -directory ${axis_dir} *]]
+}
+
+proc ::feed_reader::classifier::register_label {axis label} {
+
+    set re {^[[:alnum:]]+$}
+    if { ![regexp -- ${re} ${axis}] } {
+	error "axis name must be an alphanumeric string"
+    }
+
+    if { ![regexp -- ${re} ${label}] } {
+	error "label name must be an alphanumeric string"
+    }
+
+    set classifier_dir [get_classifier_dir]
+    set axis_dir ${classifier_dir}/${axis}
+    set label_dir ${axis_dir}/${label}
+
+    if { ![file isdirectory ${axis_dir}] } {
+	puts "${axis} is not a registered axis name"
+	puts "registered axis names:"
+	puts [join [get_axis_names] "\n"]
+	puts ---
+	error "${axis} is not a registered axis name"
+    }
+
+
+
+    file mkdir ${label_dir}
+
+}
+
+proc ::feed_reader::classifier::label {axis label contentsha1_list} {
+
+    set classifier_dir [get_classifier_dir]
+    set axis_dir ${classifier_dir}/${axis}
+    set label_dir ${axis_dir}/${label}
+
+    if { ${axis} eq {} } {
+	error "axis name cannot be empty string"
+    }
+
+    if { ${label} eq {} } {
+	error "label name cannot be empty string"
+    }
+
+    if { ![file isdirectory ${axis_dir}] } {
+	puts "${axis} is not a registered axis name"
+	puts "registered axis names:"
+	puts [join [get_axis_names] "\n"]
+	puts ---
+	error "${axis} is not a registered axis name"
+    }
+
+    if { ![file isdirectory ${label_dir}] } {
+	puts "${label} is not a registered label name"
+	puts "registered axis names:"
+	puts [join [get_label_names ${axis}] "\n"]
+	puts ---
+	error "${label} is not a registered label name"
+    }
+
+    set content_dir [::feed_reader::get_content_dir]
+    set contentsha1_to_label_dir [::feed_reader::get_contentsha1_to_label_dir]
+
+    if { ![file isdirectory ${contentsha1_to_label_dir}] } {
+	file mkdir ${contentsha1_to_label_dir}
+    }
+
+    foreach contentsha1 ${contentsha1_list} {
+	if { ![file exists ${content_dir}/${contentsha1}] } {
+	    puts "no such content item contentsha1=${contentsha1}"
+	    continue
+	}
+
+	# touch file
+	close [open ${label_dir}/${contentsha1} "w"]
+
+	set fp [open ${contentsha1_to_label_dir}/${contentsha1} "a"]
+	puts $fp ${axis}/${label}
+	close $fp
+    }
+
+}
+
+
+
+proc ::feed_reader::classifier::unlabel {axis label contentsha1_list} {
+
+    set classifier_dir [get_classifier_dir]
+    set axis_dir ${classifier_dir}/${axis}
+    set label_dir ${axis_dir}/${label}
+
+    if { ${axis} eq {} } {
+	error "axis name cannot be empty string"
+    }
+
+    if { ${label} eq {} } {
+	error "label name cannot be empty string"
+    }
+
+    if { ![file isdirectory ${axis_dir}] } {
+	puts "${axis} is not a registered axis name"
+	puts "registered axis names:"
+	puts [join [get_axis_names] "\n"]
+	puts ---
+	error "${axis} is not a registered axis name"
+    }
+
+    if { ![file isdirectory ${label_dir}] } {
+	puts "${label} is not a registered label name"
+	puts "registered axis names:"
+	puts [join [get_label_names ${axis}] "\n"]
+	puts ---
+	error "${label} is not a registered label name"
+    }
+
+    set content_dir [::feed_reader::get_content_dir]
+    set contentsha1_to_label_dir [::feed_reader::get_contentsha1_to_label_dir]
+
+    if { ![file isdirectory ${contentsha1_to_label_dir}] } {
+	file mkdir ${contentsha1_to_label_dir}
+    }
+
+    foreach contentsha1 ${contentsha1_list} {
+	if { ![file exists ${content_dir}/${contentsha1}] } {
+	    puts "no such content item contentsha1=${contentsha1}"
+	    continue
+	}
+
+
+	file delete ${label_dir}/${contentsha1}
+
+	set filename ${contentsha1_to_label_dir}/${contentsha1}
+	set indexdata [::util::readfile ${filename}]
+	set indexdata [lsearch -inline -all -not ${indexdata} ${axis}/${label}]
+	if { ${indexdata} eq {} } {
+	    file delete ${filename}
+	} else {
+	    ::util::writefile ${filename} ${indexdata}
+	}
+    }
+
+}
---
+++ lib/feed_procs/tcl/xpathfunc_procs.tcl
@@ -0,0 +1,51 @@
+
+namespace eval ::dom::xpathFunc {
+
+    array set mapping [list]
+
+    set dir [file dirname [info script]]
+    set mapping_dir [file normalize [file join ${dir} .. data]]
+
+    set filelist [glob -nocomplain -directory ${mapping_dir} *]
+
+    foreach filename ${filelist} {
+	set locale [string trimleft [file extension ${filename}] {.}]
+	set mapping(${locale}) [::util::readfile ${filename}]
+    }
+
+}
+
+
+proc ::dom::xpathFunc::normalizedate {ctxNode pos nodeListNode nodeList args} {
+
+    if { [llength ${args}] != 4 } {
+	error "normalizedate(string): wrong # of args"
+    }
+
+
+    lassign ${args} \
+	arg1Typ arg1Value \
+	arg2Typ arg2Value
+
+
+    set ts_string [::dom::xpathFuncHelper::coerce2string ${arg1Typ} ${arg1Value}]
+    set locale $arg2Value
+
+    variable mapping
+
+    if { [info exists mapping(${locale})] } {
+
+	set ts_string [string trim [string map $mapping(${locale}) ${ts_string}]]
+
+    }
+
+    if { ${ts_string} eq {now} } {
+	set ts_string [clock format [clock seconds] -format "%Y%m%dT%H%M"]
+    } elseif { [lindex ${ts_string} end] eq {ago} } {
+	# TODO: convert pretty age to a timestamp
+	set ts_string [::util::dt::age_to_timestamp ${ts_string} [clock seconds]]
+    }
+
+    return [list string ${ts_string}]
+
+}
