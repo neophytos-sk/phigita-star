a0349af15bb2a59b62b481f9f52407e6affbe8d8
Neophytos Demetriou <neophytos@gmail.com>
2013-08-20 07:55:22 +0300

    feed reader: renamed package from feed_procs to feed_reader

--- lib/feed_reader/pkgIndex.tcl
+++ lib/feed_reader/pkgIndex.tcl
@@ -2,5 +2,5 @@
 set dir [file dirname [info script]]
 
 package ifneeded feed_reader 0.1 \
-    "source [list [file join $dir tcl module-feed_procs.tcl]]"
+    "source [list [file join $dir tcl module-feed_reader.tcl]]"
 
