84b5d650448b5d2770ecd99e8389166b4fc57c00
Neophytos Demetriou <neophytos@gmail.com>
2014-02-17 02:12:38 +0200

    critbit_tree: added geopip-test.tcl

---
+++ lib/critbit_tree/test/geoip-test.tcl
@@ -0,0 +1,23 @@
+source ../../naviserver_compat/tcl/module-naviserver_compat.tcl
+
+::xo::lib::require geoip
+::xo::geoip::init
+
+
+# phigita.net
+# google.com
+# google.com.cy
+set addresses {
+    213.7.230.145
+    195.14.151.20
+    173.194.40.20
+}
+
+foreach query_ip $addresses {
+    set location_id [::xo::geoip::ip_locate $query_ip lo high]
+    if { ${location_id} ne {} } {
+        puts "location_id=$location_id lo=$lo high=$high"
+    } else {
+        puts "location for ip=\"$query_ip\" not found"
+    }
+}
