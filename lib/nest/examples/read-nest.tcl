4f63f6ff19fcacda33209867fbf197f5723b5fd7
Neophytos Demetriou <neophytos@azet.sk>
2014-11-26 11:56:57 +0100

    nest-lang: check script args in read-nest.tcl

--- lib/nest/examples/read-nest.tcl
+++ lib/nest/examples/read-nest.tcl
@@ -1,6 +1,12 @@
+#!/usr/bin/tclsh
 set dir [file dirname [info script]]
 lappend auto_path [file join $dir ".."]
 package require nest
+
+if { [llength $argv] != 1 } {
+    puts "Usage: $argv0 message.nest"
+    exit
+}
 
 set filename [lindex $argv 0]
 set lang_nsp ::nest::data
